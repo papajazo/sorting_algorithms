def8956ff2453f03c40e8b4896dce51e0f323a7a
ctrlouma <bygeoffreyouma@gmail.com>
2023-06-25 08:54:56 -0700

    bubble sort array

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order,
+ * using the Buble sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int r, sorted_array = 0;
+
+	if (size < 2)
+		return;
+
+	while (sorted_array == 0)
+	{
+		sorted_array = 1;
+		for (; r < (size - 1); r++)
+		{
+			if (array[r] > array[r + 1])
+			{
+				swap_array_element(&array[r], &array[r + 1]);
+				print_array(array, size);
+				sorted_array = 0;
+			}
+		}
+	}
+}
+
+/*
+ * swap_array_element - swaps two elements of @array
+ *
+ * @m: first element
+ * @n: second element
+ */
+void swap_array_element(int *m, int *n)
+{
+	int o = 0;
+
+	o = *m;
+	*m = *n;
+	*n = o;
+}
--- sort.h
+++ sort.h
@@ -30,8 +30,9 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+/* void sort_deck(deck_node_t **deck); */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void swap_array_element(int *m, int *n);
 
 #endif
